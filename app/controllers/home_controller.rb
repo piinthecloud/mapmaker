35ee169bcb8e1bea0b9e85565e6a71cac7c0b008
stephanie pi <stephmpi@gmail.com>
2015-01-09 12:04:03 -0800

    test db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
 
 
   def index
+    Policeshootings.where(:city.ne => nil).each do |incident|
+      @path = (URI.encode("https://maps.googleapis.com/maps/api/geocode/json?address=#{incident.city.gsub!(" ", "-")||incident.city}&components=country:US|administrative_area:#{incident.state[0..1]}"))
+
+      response = HTTParty.get(@path)
+      incident.set(:lat => response['results'][0]['geometry']['location']['lat'] )
+      incident.set(:lng => response['results'][0]['geometry']['location']['lng'] )
+    end
+
     @data = Policeshootings.where(:city.ne => nil)
 
   end
