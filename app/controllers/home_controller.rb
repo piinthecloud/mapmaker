79931762b85d6a516c545f278907a17ff4cf492a
stephanie pi <stephmpi@gmail.com>
2015-01-09 12:00:12 -0800

    add mongolab

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 
 end
 
-#
+# #
 # Policeshootings.where(:city.ne => nil).each do |incident|
 #   @path = (URI.encode("https://maps.googleapis.com/maps/api/geocode/json?address=#{incident.city.gsub!(" ", "-")||incident.city}&components=country:US|administrative_area:#{incident.state[0..1]}"))
 #
