ef4ff7f1b7d51767a55caf2cefc1c38fad2fb55a
stephanie pi <stephmpi@gmail.com>
2015-01-09 13:00:55 -0800

    modify seed to destroy all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Policeshootings.destroy_all
+
 JSON.parse(File.read("public/data.json")).each do |x|
   Policeshootings.create( x )
 end
