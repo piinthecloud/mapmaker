9a1071a1396158173cfce325ffa3911d9837a87d
stephanie pi <stephmpi@gmail.com>
2015-01-09 16:14:04 -0800

    style map

--- app/models/policeshootings.rb
+++ app/models/policeshootings.rb
@@ -4,6 +4,7 @@ class Policeshootings
   def self.add_locations
     Policeshootings.where(:city.ne => nil, :lat => nil).each do |incident|
       @path = (URI.encode("https://maps.googleapis.com/maps/api/geocode/json?address=#{incident.city.gsub!(" ", "-")||incident.city}&components=country:US|administrative_area:#{incident.state[0..1]}"))
+      sleep 0.2
 
       response = HTTParty.get(@path)
       incident.set(:lat => response['results'][0]['geometry']['location']['lat'] )
